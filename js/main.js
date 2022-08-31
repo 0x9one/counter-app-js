07de54ce3f43395de3c1a3bf257e710eead5d2ba
byeweb2 <sy47525@gmail.com>
2022-08-31 22:41:16 +0100

    Create save button and add save paragraph to render the count we save

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 let countEl = document.getElementById("count-el");
+let saveEL = document.getElementById("save-el");
 let count = 0;
 function increment() {
-    count = count + 1;
+    // Use the plus equal technique
+    count += 1;
     countEl.innerText = count;
+}
+// Create a save() function
+function save() {
+    let preveEntire = count + " -";
+    saveEL.innerText += preveEntire;
 }
